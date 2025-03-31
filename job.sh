f1e7f7d85fae86844517a27f1731e3a7a73bfb96
juan <juanpajaro@javeriana.edu.co>
2025-03-31 04:52:34 -0500

    se modifico el job para que sea multiprocessing y no cargue MPI, dado que el cluster no lo permite

--- job.sh
+++ job.sh
@@ -2,22 +2,18 @@
 #SBATCH --job-name=mi_tarea_paralela       # Nombre del trabajo
 #SBATCH --output=resultados/salida_%j.out  # Archivo de salida en la carpeta resultados
 #SBATCH --error=resultados/error_%j.err    # Archivo de errores en la carpeta resultados
-#SBATCH --nodes=4                          # Número de nodos solicitados
-#SBATCH --ntasks=1                         # Número total de tareas (una sola tarea en varios nodos)
-#SBATCH --ntasks-per-node=1                 # Una tarea por nodo
-#SBATCH --cpus-per-task=16                 # Número de núcleos por tarea
+#SBATCH --nodes=1                          # Usar un solo nodo (worker)
+#SBATCH --ntasks=1                         # Una sola tarea
+#SBATCH --cpus-per-task=16                 # Número de núcleos a usar (ajustar según el nodo)
 #SBATCH --time=01:00:00                    # Tiempo máximo de ejecución
-##SBATCH --partition=general                # Partición a utilizar
+#SBATCH --partition=general                # Partición a utilizar
 
 # Cargar el entorno Conda
-source /zine/apps/anaconda_salud/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 conda activate mpi_env
 
 # Crear carpeta de resultados si no existe
 mkdir -p resultados
 
-# Cargar el entorno MPI
-module load mpi
-
 # Ejecutar la tarea con srun
-srun --mpi=pmix python /zine/data/salud/slurm_pruebas/mi_programa_mpi.py
+srun --mpi=pmix python /zine/data/salud/slurm_pruebas/mi_programa_multiprocessing.py
